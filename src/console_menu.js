1382b131f8de16f1ceca7a3346362a53cfcebf8c
evgeny.konoplev <evgeny.konoplev@gemotest.ru>
2019-10-08 12:55:27 +0300

    some refactoring

---
+++ src/console_menu.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const commandLineArgs = require('command-line-args');
+const Help = require('./components/help.js');
+const TableViewer = require('./components/table_viewer.js');
+const FixtureLoader = require('./components/fixture_loader.js');
+
+const optionDefinitions = [
+    { name: 'verbose', alias: 'v', type: Boolean },
+    { name: 'mode', alias: 'm', type: String, defaultOption: true },
+    { name: 'table', type: String },
+    { name: 'top', type: Number },
+    { name: 'fixture', alias: 'f', type: String },
+    { name: 'directory', alias: 'd', type: String },
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+const ConsoleMenu = (config) => {
+    if (options.mode === 'help') {
+        Help.print();
+    } else if (options.mode === 'view') {
+        TableViewer.view(config.db, options.table, options.top);
+    } else if (options.mode === 'load') {
+        FixtureLoader.loadFile(config, options.fixture);
+    } /*else if (options.mode === 'unload') {
+        FixtureLoader.unloadFile(options.fixture);
+    } else if (options.mode === 'load-all') {
+        FixtureLoader.loadDirectory(options.directory);
+    } else if (options.mode === 'unload-all') {
+        FixtureLoader.unloadDirectory(options.directory);
+    }*/ else {
+        console.log('dir:', __dirname);
+        Help.print();
+    }
+}
+
+module.exports = ConsoleMenu;
